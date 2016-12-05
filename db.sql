b6a4968099e953905b784b26b492a3885eb81691
ajstharsan-radus28 <ajstharsan@gmail.com>
2016-12-05 21:48:15 +0530

    update db.sql auto increment sequence
    Update auto increment values

--- db.sql
+++ db.sql
@@ -1,6 +1,6 @@
-INSERT INTO `vtiger_ws_operation` (`operationid`, `name`, `handler_path`, `handler_method`, `type`, `prelogin`) VALUES ('34', 'retrievelanguages', 'include/Webservices/RetrieveLanguages.php', 'vtws_retrievelanguages', 'GET', '0');
-
-UPDATE `vtiger_ws_operation_seq` SET `id` = '34' WHERE `id` = '33';
+INSERT INTO `vtiger_ws_operation` (`operationid`, `name`, `handler_path`, `handler_method`, `type`, `prelogin`) VALUES ((SELECT id FROM `vtiger_ws_operation_seq`), 'retrievelanguages', 'include/Webservices/RetrieveLanguages.php', 'vtws_retrievelanguages', 'GET', '0');
 
 INSERT INTO `vtiger_ws_operation_parameters` (`operationid`, `name`, `type`, `sequence`) VALUES 
-('34', 'lang', 'String', '1'),('34', 'modules', 'String', '1'); 
+((SELECT id FROM `vtiger_ws_operation_seq`), 'lang', 'String', '1'),((SELECT id FROM `vtiger_ws_operation_seq`), 'modules', 'String', '1'); 
+
+UPDATE `vtiger_ws_operation_seq` SET `id` = (id+1);
