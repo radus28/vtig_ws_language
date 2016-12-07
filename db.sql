8123687aa769a7129b6818748d5f4aa422c78221
ajstharsan-radus28 <ajstharsan@gmail.com>
2016-12-07 23:31:48 +0530

    Update db.sql

--- db.sql
+++ db.sql
@@ -1,6 +1,8 @@
+UPDATE `vtiger_ws_operation_seq` SET `id` = (id+1);
 INSERT INTO `vtiger_ws_operation` (`operationid`, `name`, `handler_path`, `handler_method`, `type`, `prelogin`) VALUES ((SELECT id FROM `vtiger_ws_operation_seq`), 'retrievelanguages', 'include/Webservices/RetrieveLanguages.php', 'vtws_retrievelanguages', 'GET', '0');
 
 INSERT INTO `vtiger_ws_operation_parameters` (`operationid`, `name`, `type`, `sequence`) VALUES 
-((SELECT id FROM `vtiger_ws_operation_seq`), 'lang', 'String', '1'),((SELECT id FROM `vtiger_ws_operation_seq`), 'modules', 'String', '1'); 
+((SELECT id FROM `vtiger_ws_operation_seq`), 'lang', 'String', '1'),
+((SELECT id FROM `vtiger_ws_operation_seq`), 'modules', 'String', '2'); 
 
-UPDATE `vtiger_ws_operation_seq` SET `id` = (id+1);
+
